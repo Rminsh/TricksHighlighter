d9e3c54308db8312a1cbcb7823995c78c1c0e5d2
Armin <armin536@gmail.com>
2022-07-05 20:03:07 +0430

    Initial Commit

---
+++ TricksHighlighter/TricksHighlighterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TricksHighlighterApp.swift
+//  TricksHighlighter
+//
+//  Created by Armin on 7/5/22.
+//
+
+import SwiftUI
+
+@main
+struct TricksHighlighterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
