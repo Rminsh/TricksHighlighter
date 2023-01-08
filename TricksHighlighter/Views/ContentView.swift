ce7f106003332fe338f840940c0d24bbe0dea727
Armin <armin536@gmail.com>
2023-01-08 22:50:38 +0330

    Changed packages to use only Highlighter

--- TricksHighlighter/Views/ContentView.swift
+++ TricksHighlighter/Views/ContentView.swift
@@ -7,7 +7,6 @@
 
 import SwiftUI
 import Highlighter
-import CodeEditor
 
 struct ContentView: View {
     
--- TricksHighlighter/Views/FileThumbnail.swift
+++ TricksHighlighter/Views/FileThumbnail.swift
@@ -6,7 +6,6 @@
 //
 
 import SwiftUI
-import CodeEditor
 import Highlighter
 
 struct FileThumbnail: View {
